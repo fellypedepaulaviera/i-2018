38982eaaae561e45feab80a5d98fc0fee3fe2b26
Jhordan Gabriel <jhordangabriel@hotmail.com>
2018-04-02 20:32:38 -0300

    exericio 1 - Lista 2

---
+++ src/i2018/I2018.java
@@ -0,0 +1,20 @@
+package i2018;
+
+import br.com.ufg.inf.lista1.exercicio1.Exercicio1;
+import java.io.IOException;
+
+/**
+ *
+ * @author Jhordan Gabriel
+ */
+public class I2018 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        System.out.println(Exercicio1.primeiros4HexaDoArquivo("C:\\test\\file.txt"));
+    }
+    
+}
